5850257d6d58d243c813dca84dd554572ae55191
Agam Rafaeli <agam@tapingo.com>
2015-12-26 14:24:29 +0200

    Add running DB in memory vs from file + documentation in README file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Investa_Gator
 
-Welcome to the Investa_Gator repo
+
+## Environment Variables
+
+Default settings for the Flask's app database location
+```
+FLASK_DB_IN_MEMORY_FLAG=True
+```
+
+If you want to run this over a file database use these settings:
+```
+FLASK_DB_IN_MEMORY_FLAG=False
+DATABASE_URL = "/path/to/db/db_file_name.db" 
+```
--- full_app/app/__init__.py
+++ full_app/app/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
+# Define the app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db" #os.environ["DATABASE_URL"]
+
+# Define the database
+USE_IN_MEMORY_FLAG = os.getenv("FLASK_DB_IN_MEMORY_FLAG", "True")
+USE_IN_MEMORY = USE_IN_MEMORY_FLAG == "True"
+if USE_IN_MEMORY:
+    DATABASE_URI = "/"
+else:
+    DATABASE_URI = os.getenv("FLASK_DATABASE_URI", "/")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DATABASE_URI
+print "Database uri in use is " + str(app.config["SQLALCHEMY_DATABASE_URI"])
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
